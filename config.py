2ac27357ac2ae60c7356554dd50db73b27648f54
zarymisa131 <87584067+Misazary@users.noreply.github.com>
2022-10-29 18:08:10 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
 DOWNLOAD_LOCATION = "./Downloads"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "asddsadsd")
-BOT_TOKEN = "5602656546:AAHqVeAqQqgMaZtH9I91iFG-eGUPnJe-1HM"
-APP_ID = int(os.environ.get("API_ID", "sdasds"))d
-APP_ID = "17097734"
-API_HASH = os.environ.get("API_HASH", "dsfdfsf")
+BOT_TOKEN = os.environ.get("5602656546:AAHqVeAqQqgMaZtH9I91iFG-eGUPnJe-1HM", "asddsadsd")
+APP_ID = int(os.environ.get("17097734", "sdasds"))d
+APP_ID =  = os.environ.get("5315b4520ff9b0289dc2a5fb8aabad54", "dsfdfsf")
 API_HASH = "5315b4520ff9b0289dc2a5fb8aabad54"
 AUTH_GROUP = ""
 
